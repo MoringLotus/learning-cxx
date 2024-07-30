d0d71fec22cd0375763ae5a8f9dfcfa677212493
unknown <MoringLotus>
2024-07-30 19:49:40 +0800

    25 q

--- exercises/23_std_vector/main.cpp
+++ exercises/23_std_vector/main.cpp
@@ -10,7 +10,8 @@ int main(int argc, char **argv) {
         std::vector<int> vec{1, 2, 3, 4, 5};
         ASSERT(vec.size() == 5, "Fill in the correct value.");
         // THINK: `std::vector` 的大小是什么意思？与什么有关？
-        ASSERT(sizeof(vec) == 20, "Fill in the correct value.");
+        std::cout << sizeof(vec) << std::endl;
+        ASSERT(sizeof(vec) == 24, "Fill in the correct value.");
         int ans[]{1, 2, 3, 4, 5};
         ASSERT(std::memcmp(vec.data(), ans, sizeof(ans)) == 0, "Fill in the correct values.");
     }
@@ -18,40 +19,40 @@ int main(int argc, char **argv) {
         std::vector<double> vec{1, 2, 3, 4, 5};
         {
             ASSERT(vec.size() == 5, "Fill in the correct value.");
-            ASSERT(sizeof(vec) == 20, "Fill in the correct value.");
+            ASSERT(sizeof(vec) == 24, "Fill in the correct value.");
             double ans[]{1, 2, 3, 4, 5};
             ASSERT(std::memcmp(vec.data(), ans, sizeof(ans)) == 0, "Fill in the correct values.");
         }
         {
             vec.push_back(6);
             ASSERT(vec.size() == 6, "Fill in the correct value.");
-            ASSERT(sizeof(vec) == 20, "Fill in the correct value.");
+            ASSERT(sizeof(vec) == 24, "Fill in the correct value.");
             vec.pop_back();
             ASSERT(vec.size() == 5, "Fill in the correct value.");
-            ASSERT(sizeof(vec) == 20, "Fill in the correct value.");
+            ASSERT(sizeof(vec) == 24, "Fill in the correct value.");
         }
         {
             vec[4] = 6;
             ASSERT(vec[0] == 1, "Fill in the correct value.");
             ASSERT(vec[1] == 2, "Fill in the correct value.");
             ASSERT(vec[2] == 3, "Fill in the correct value.");
-            ASSERT(vec[3] == 6, "Fill in the correct value.");
-            ASSERT(vec[4] == 5, "Fill in the correct value.");
+            ASSERT(vec[3] == 4, "Fill in the correct value.");
+            ASSERT(vec[4] == 6, "Fill in the correct value.");
         }
         {
             // THINK: `std::vector` 插入删除的时间复杂度是什么？
-            vec.insert(1, 1.5);
+            vec.insert(vec.begin() + 1, 1.5);
             ASSERT((vec == std::vector<double>{1, 1.5, 2, 3, 4, 6}), "Make this assertion pass.");
-            vec.erase(4);
+            vec.erase(vec.begin() + 3);
             ASSERT((vec == std::vector<double>{1, 1.5, 2, 4, 6}), "Make this assertion pass.");
         }
         {
             vec.shrink_to_fit();
-            ASSERT(vec.capacity() == 0, "Fill in the correct value.");
+            ASSERT(vec.capacity() == 5, "Fill in the correct value.");
             vec.clear();
             ASSERT(vec.empty(), "`vec` is empty now.");
             ASSERT(vec.size() == 0, "Fill in the correct value.");
-            ASSERT(vec.capacity() == 0, "Fill in the correct value.");
+            ASSERT(vec.capacity() == 5, "Fill in the correct value.");
         }
     }
     {
@@ -59,12 +60,12 @@ int main(int argc, char **argv) {
         ASSERT(vec[0] == 'z', "Make this assertion pass.");
         ASSERT(vec[47] == 'z', "Make this assertion pass.");
         ASSERT(vec.size() == 48, "Make this assertion pass.");
-        ASSERT(sizeof(vec) == 20, "Fill in the correct value.");
+        ASSERT(sizeof(vec) == 24, "Fill in the correct value.");
         {
             auto capacity = vec.capacity();
             vec.resize(16);
             ASSERT(vec.size() == 16, "Fill in the correct value.");
-            ASSERT(vec.capacity() == 16, "Fill in a correct identifier.");
+            ASSERT(vec.capacity() == 48, "Fill in a correct identifier.");
         }
         {
             vec.reserve(256);
